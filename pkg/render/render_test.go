5afb578a8208c00b13701feedf2c93e607395dbf
bob(taebinlim) <bob@bc-labs.net>
2023-07-20 09:33:06 +0900

    test: :test_tube: rendering inject key template

---
+++ pkg/render/render_test.go
@@ -0,0 +1,43 @@
+package render
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/InfraBlockchain/infrablockspace-operator/pkg/chain"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestRenderInjectKeyTemplate(t *testing.T) {
+
+	keys := []chain.Key{
+		{KeyType: "gran", Scheme: "test2", Seed: "test2"},
+		{KeyType: "test", Scheme: "test", Seed: "test"},
+	}
+	t.Run("create key", func(t *testing.T) {
+		got := RenderingInjectKeyTemplate(keys)
+		fmt.Print(got)
+	})
+}
+
+func TestRenderInjectKeyRange(t *testing.T) {
+
+	keys := []*chain.Key{
+		{KeyType: "gran", Scheme: "test2", Seed: "test2"},
+		{KeyType: "test", Scheme: "test", Seed: "test"},
+	}
+
+	volumeMounts := []corev1.VolumeMount{
+		{
+			Name:      "chain-keystore",
+			MountPath: "/keystore",
+		},
+	}
+	for _, key := range keys {
+		volumeMounts = append(volumeMounts, corev1.VolumeMount{
+			Name:      key.KeyType,
+			MountPath: fmt.Sprintf("/var/run/secrets/%s", key.KeyType),
+		})
+	}
+	fmt.Println(volumeMounts)
+}
