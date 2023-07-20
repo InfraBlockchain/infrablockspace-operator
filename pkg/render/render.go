d2e3dc5d32f539d4b6ed5da88b5179690429120b
bob(taebinlim) <bob@bc-labs.net>
2023-07-20 16:27:00 +0900

    refactor: :recycle: resolve infinite chainging render to chain

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,38 +2,18 @@ package render
 
 import (
 	"bytes"
-	"html/template"
-
-	"github.com/InfraBlockchain/infrablockspace-operator/pkg/chain"
 	"github.com/tae2089/bob-logging/logger"
+	"html/template"
 )
 
-const InjectKeyScript string = `
-          - |
-            set -eu
-		{{range .}}
-            if [ ! -f/var/run/secrets/{{.KeyType}}/type ]; then
-               echo "Error: File/var/run/secrets/{{.KeyType}}/type does not exist"
-               exit 1
-            fi
-           /infra-relay-chain/infrablockspace key insert \
-           --keystore-path /keystore \
-           --key-type $(cat /var/run/secrets/{{.KeyType}}/type) \
-           --scheme $(cat /var/run/secrets/{{.KeyType}}/scheme) \
-           --suri /var/run/secrets/{{.KeyType}}/seed \
-           && echo "Inserted key into Keystore" \
-           || echo "Failed to insert key into Keystore."
-		{{end}}
-`
-
-func RenderingInjectKeyTemplate(Keys []chain.Key) string {
+func RenderingInTemplate(InjectKeyScript string, data any) string {
 	tmpl, err := template.New("Create Job").Parse(InjectKeyScript)
 	if err != nil {
 		logger.Error(err)
 		return ""
 	}
 	var tpl bytes.Buffer
-	if err := tmpl.Execute(&tpl, Keys); err != nil {
+	if err := tmpl.Execute(&tpl, data); err != nil {
 		logger.Error(err)
 		return ""
 	}
