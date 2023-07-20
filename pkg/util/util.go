4fd6b7409b15076860afa933bf29ddd4b0135513
bob(taebinlim) <bob@bc-labs.net>
2023-07-20 16:28:07 +0900

    feature: :seedling: create base63decoding, generateResourceName functions

---
+++ pkg/util/util.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"encoding/base64"
+	"github.com/tae2089/bob-logging/logger"
+	"strings"
+)
+
+func GenerateResourceName(names ...string) string {
+	newNameList := make([]string, 0)
+	for _, name := range names {
+		if name != "" {
+			newNameList = append(newNameList, name)
+		}
+	}
+	return strings.Join(newNameList, "-")
+}
+
+func DecodingBase64(data []byte) string {
+	output, err := base64.StdEncoding.DecodeString(string(data))
+	if err != nil {
+		logger.Error(err)
+		return ""
+	}
+	return string(output)
+}
