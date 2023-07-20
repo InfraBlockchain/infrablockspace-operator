ff76e8aa26de25eefc59d2207a18dbb89d03674a
bob(taebinlim) <bob@bc-labs.net>
2023-07-20 16:28:36 +0900

    feature: :seedling: create pvc functions

--- pkg/chain/pvc.go
+++ pkg/chain/pvc.go
@@ -1,1 +1,38 @@
 package chain
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+)
+
+func CreateChainPVC(name, namespace, size, scName string) *corev1.PersistentVolumeClaim {
+	pvc := generatePVC(name, namespace, size, scName)
+	return pvc
+}
+
+func generatePVC(name, namespace, size, scName string) *corev1.PersistentVolumeClaim {
+	return &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels: map[string]string{
+				"app": name,
+			},
+		},
+		Spec: corev1.PersistentVolumeClaimSpec{
+			AccessModes:      []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+			StorageClassName: pointer.String(scName),
+			Resources: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceStorage: resource.MustParse(size),
+				},
+			},
+		},
+	}
+}
+
+func IsSamePvcSize(quantity1, quantity2 resource.Quantity) bool {
+	return quantity1 == quantity2
+}
