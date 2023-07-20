b66db824cbb0dd0aa98de40e2c6f7370b929f6b1
bob(taebinlim) <bob@bc-labs.net>
2023-07-20 16:29:56 +0900

    feature: :seedling: define chain specs

---
+++ pkg/chain/spec.go
@@ -0,0 +1,59 @@
+// Package chain is package for common chain related functions and types
+package chain
+
+// Port is a struct for chain port information
+type Port struct {
+	RPCPort int32 `json:"rpcPort,omitempty"`
+	WSPort  int32 `json:"wsPort,omitempty"`
+	P2PPort int32 `json:"p2pPort,omitempty"`
+}
+
+// Key is a struct for chain key information
+type Key struct {
+	KeyType string `json:"type,omitempty"`
+	Scheme  string `json:"scheme,omitempty"`
+	Seed    string `json:"seed,omitempty"`
+}
+
+// KeyStore is volume name for chain key store
+const KeyStore string = "chain-keystore"
+
+// Spec is volume name for chain spec
+const Spec string = "chain-spec"
+
+// SpecMountPath is volume mount path for chain spec
+const SpecMountPath string = "/tmp"
+
+// KeyStoreMountPath is volume mount path for chain key store
+const KeyStoreMountPath string = "/keystore"
+
+const DownloadChainSpecImage = "curlimages/curl:8.1.2"
+
+const RelayChainSpecFileName = "relay-chain-spec.json"
+
+const DownloadRelayChainSpecContainer = "download-relay-chain-spec"
+
+const InjectKeysContainer = "inject-keys"
+
+type ChainType string
+
+const RelayChain ChainType = "relay"
+const ParaChain ChainType = "para"
+
+const InjectKeyScript string = `
+          - |
+            set -eu
+		{{range .}}
+            if [ ! -f/var/run/secrets/{{.KeyType}}/type ]; then
+               echo "Error: File/var/run/secrets/{{.KeyType}}/type does not exist"
+               exit 1
+            fi
+           /infra-relay-chain/infrablockspace key insert \
+           --keystore-path /keystore \
+           --key-type $(cat /var/run/secrets/{{.KeyType}}/type) \
+           --scheme $(cat /var/run/secrets/{{.KeyType}}/scheme) \
+           --suri /var/run/secrets/{{.KeyType}}/seed \
+           && echo "Inserted key into Keystore" \
+           || echo "Failed to insert key into Keystore."
+		{{end}}
+`
