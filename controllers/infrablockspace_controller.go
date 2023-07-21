b975f5c6182575ba961803bd797646f76e2d10bd
bob(taebinlim) <bob@bc-labs.net>
2023-07-21 13:58:14 +0900

    feature: :seedling: add line for pvc permision (#12)

--- controllers/infrablockspace_controller.go
+++ controllers/infrablockspace_controller.go
@@ -50,6 +50,7 @@ type InfraBlockSpaceReconciler struct {
 // +kubebuilder:rbac:groups=core,resources=pods/logs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
