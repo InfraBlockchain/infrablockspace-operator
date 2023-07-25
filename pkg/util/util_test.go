273c863b851caad644c6030d55e27b1953419d58
bob(taebinlim) <bob@bc-labs.net>
2023-07-25 09:12:21 +0900

    test: :test_tube: test generate name (#17)

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -17,3 +17,21 @@ func TestDecodingBase64(t *testing.T) {
 		assert.Equal(t, "", result)
 	})
 }
+
+func TestEncodingBase64(t *testing.T) {
+	data := []string{"test", "an3", "b"}
+	t.Run("generating name test 1", func(t *testing.T) {
+		result := GenerateResourceName(data...)
+		assert.Equal(t, "test-an3-b", result)
+	})
+	data = []string{"test", "", "b"}
+	t.Run("generating name test 2", func(t *testing.T) {
+		result := GenerateResourceName(data...)
+		assert.Equal(t, "test-b", result)
+	})
+	t.Run("generating name test 3", func(t *testing.T) {
+		result := GenerateResourceName(data...)
+		result = result + "-service"
+		assert.Equal(t, "test-b-service", result)
+	})
+}
